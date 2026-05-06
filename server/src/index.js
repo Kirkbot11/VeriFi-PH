6ae493d2452973051c5f236c91f76b122844dd55
kirkbot11 <encalladokirkandrei@gmail.com>
2026-05-07 00:56:46 +0800

    fix cors for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -18,9 +18,8 @@ const app = express();
  * MIDDLEWARE
  * -------------------------
  */
-app.use(helmet());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true
 }));
@@ -83,12 +82,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true
   }
 });
-
 io.on('connection', (socket) => {
   console.log(`🟢 Client connected: ${socket.id}`);
 
